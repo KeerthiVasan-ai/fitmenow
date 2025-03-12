f4720e6b32a6c0aa70fc02318fcbb79270ca4de0
KeerthiVasan-ai <keerthi77459@gmail.com>
2025-03-12 15:48:17 +0530

    fix

--- DeepFashion_Try_On/ACGPN_inference/inference.py
+++ DeepFashion_Try_On/ACGPN_inference/inference.py
@@ -42,15 +42,15 @@ def apply_dresses(person_image, label, mask, pose, dresses):
         img_fore_wc = img_fore * mask_fore
         all_clothes_label = changearm(label)
 
-        dress_color = dress["color"].permute(1, 2, 0).cpu().numpy()  # Convert to (H, W, C)
-        dress_color = (dress_color + 1) / 2  # Normalize from [-1,1] to [0,1]
+        # dress_color = dress["color"].permute(1, 2, 0).cpu().numpy()  # Convert to (H, W, C)
+        # dress_color = (dress_color + 1) / 2  # Normalize from [-1,1] to [0,1]
 
-        # Plot the dress color image
-        plt.figure(figsize=(3, 3))
-        plt.imshow(dress_color)
-        plt.axis("off")
-        plt.title(f"Recommendation {i+1}")
-        plt.show()
+        # # Plot the dress color image
+        # plt.figure(figsize=(3, 3))
+        # plt.imshow(dress_color)
+        # plt.axis("off")
+        # plt.title(f"Recommendation {i+1}")
+        # plt.show()
 
         # Forward Pass
         _, fake_image, _, _, _, _, _, _, rgb, _ = model(
@@ -83,20 +83,20 @@ def find_data_by_name(dataset, target_name):
     print("Not Found")
     return None
 
-def find_cloth_data_by_name(dataset,cloth_data:list):
-    cloths = []
-    for data in dataset:
-        if(data["name"] in cloth_data):
-            print(data["name"])
-            cloths.append(data)
-    print(cloths)
-    return cloths
+# def find_cloth_data_by_name(dataset,cloth_data:list):
+#     cloths = []
+#     for data in dataset:
+#         if(data["name"] in cloth_data):
+#             print(data["name"])
+#             cloths.append(data)
+#     print(cloths)
+#     return cloths
 
 # Load single person image and its related data
 data_loader = CreateDataLoader(opt)
 dataset = data_loader.load_data()
 
-cloth_data_list = [["003069_0.jpg"],["013245_0.jpg"],["005101_0.jpg"],["004904_0.jpg"],["010984_.jpg"]]
+# cloth_data_list = [["003069_0.jpg"],["013245_0.jpg"],["005101_0.jpg"],["004904_0.jpg"],["010984_.jpg"]]
 
 # ['/kaggle/input/viton-dataset/ACGPN_TestData/test_color/003069_1.jpg', 
 #  '/kaggle/input/viton-dataset/ACGPN_TestData/test_color/013245_1.jpg', 
@@ -105,7 +105,7 @@ cloth_data_list = [["003069_0.jpg"],["013245_0.jpg"],["005101_0.jpg"],["004904_0
 #  '/kaggle/input/viton-dataset/ACGPN_TestData/test_color/010984_1.jpg']
 
 person_data = find_data_by_name(dataset,["002103_0.jpg"])
-cloths = find_cloth_data_by_name(dataset,cloth_data_list)
+# cloths = find_cloth_data_by_name(dataset,cloth_data_list)
 
 person_image = person_data['image']
 person_label = person_data['label']
@@ -115,4 +115,4 @@ person_pose = person_data['pose']
 
 
 # Apply all dresses in the dataset to the single person image
-apply_dresses(person_image, person_label, person_mask, person_pose, cloths)
+apply_dresses(person_image, person_label, person_mask, person_pose, dataset)
